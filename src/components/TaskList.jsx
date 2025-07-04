4ed74908bc6a830f7867908d0ea601d1404a7b5b
Valeria <vdelaconcep@gmail.com>
2025-07-04 17:56:16 -0300

    Versión para entregar

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -10,7 +10,7 @@ const TaskList = ({ lista, setLista }) => {
     //Effect para cambiar estado "listaVacia" cuando la lista se vacíe
     useEffect(() => {
         if (lista.length === 0) {
-            setTimeout(setListaVacia(true), 100)
+            setTimeout(setListaVacia(true), 200)
         } else setListaVacia(false)
     },[lista, setListaVacia])
 
